d23c76cd0aa9c891bc8466848736024052dacc34
Abel Trotter, Kelli Brooke and Nate Cottle <kelli@>
2018-07-31 09:44:02 -0700

    Add complete tests for triangle logic

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -1,16 +1,52 @@
-import { Triangle } from './../src/triangle.js';
+import { Triangle } from './../src/triangle';
 
 describe('Triangle', function() {
+  var triangle;
+
+  beforeEach(function() {
+    triangle = new Triangle(5, 5, 5);
+  });
 
   it('should test whether a Triangle has three sides', function() {
-    var triangle = new Triangle(3,4,5);
-    expect(triangle.side1).toEqual(3);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).not.toEqual(6);
+    expect(triangle.side1).toEqual(5);
+    expect(triangle.side2).toEqual(5);
+    expect(triangle.side3).toEqual(5);
   });
 
   it('should correctly determine whether three lengths can be made into a triangle', function() {
     var notTriangle = new Triangle(3,9,22);
-    expect(notTriangle.checkType()).toEqual("not a triangle");
+    expect(notTriangle.isNotTriangle()).toEqual(true);
+  });
+
+  it('should test whether a shape is an isosceles triangle', function() {
+    expect(triangle.isIsosceles()).toEqual(false);
+  });
+
+  it('should test whether a shape is an equilateral triangle', function() {
+    expect(triangle.isEquilateral()).toEqual(true);
+  });
+
+  it('should test whether a shape is an scalene triangle', function() {
+    expect(triangle.isScalene()).toEqual(false);
+  });
+
+  it('should test what kind of triangle a triangle is', function() {
+    expect(triangle.checkTriangle()).toEqual("Equilateral");
+  });
+
+  it('should test what kind of triangle a triangle is', function() {
+    var aTriangle = new Triangle(4, 5, 5);
+    expect(aTriangle.checkTriangle()).toEqual("Isosceles");
+  });
+
+  it('should test what kind of triangle a triangle is', function() {
+    var aTriangle = new Triangle(7, 3, 5);
+    expect(aTriangle.checkTriangle()).toEqual("Scalene");
+  });
+
+  it('should test what kind of triangle a triangle is', function() {
+    var aTriangle = new Triangle(3,6640,22);
+    console.log(aTriangle.isNotTriangle());
+    expect(aTriangle.checkTriangle()).toEqual("Not a triangle!");
   });
 });
--- src/triangle.js
+++ src/triangle.js
@@ -4,8 +4,39 @@ export function Triangle(side1, side2, side3) {
   this.side3 = side3;
 }
 
-Triangle.prototype.checkType = function() {
+Triangle.prototype.isNotTriangle = function() {
   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
-    return "not a triangle";
+    return true;
   }
 };
+
+Triangle.prototype.isIsosceles = function() {
+  if ((this.side1 === this.side2 && this.side2 !== this.side3) || (this.side1 === this.side3 && this.side2 !== this.side3) ||
+  (this.side1 !== this.side2 && this.side2 === this.side3)) return true;
+  return false;
+};
+
+// all sides equal
+Triangle.prototype.isEquilateral = function() {
+  if (this.side1 === this.side2 && this.side1 === this.side3) return true;
+}
+
+// no sides equal
+Triangle.prototype.isScalene = function() {
+  if (this.side1 !== this.side2 && this.side2 !== this.side3) return true;
+  return false;
+}
+
+Triangle.prototype.checkTriangle = function() {
+  if (this.isNotTriangle()) {
+    return "Not a triangle!";
+  } else if (this.isIsosceles()) {
+    return "Isosceles";
+  } else if (this.isEquilateral()) {
+    return "Equilateral";
+  } else if (this.isScalene()) {
+    return "Scalene";
+  } else {
+    return "You broke it...";
+  }
+}
