ea3e923316790a8f5156e15c9ea203c6fd3095b4
Abel Trotter <a_trotter0@hotmail.com>
2018-08-01 07:56:30 -0700

    Add babel

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import { Triangle } from './triangle';
+import { Triangle } from './triangle';
 
 $(document).ready(function() {
-  console.log("hey, you should buy Abe a beer sometime!");
+  
 });
--- src/triangle.js
+++ src/triangle.js
@@ -1,42 +1,44 @@
-export function Triangle(side1, side2, side3) {
-  this.side1 = side1;
-  this.side2 = side2;
-  this.side3 = side3;
-}
+class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
 
-Triangle.prototype.isNotTriangle = function() {
-  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
-    return true;
+  isNotTriangle() {
+    if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+      return true;
+    }
+  }
+
+  isIsosceles() {
+    if ((this.side1 === this.side2 && this.side2 !== this.side3) || (this.side1 === this.side3 && this.side2 !== this.side3) ||
+    (this.side1 !== this.side2 && this.side2 === this.side3)) return true;
+    return false;
+  }
+
+  // all sides equal
+  isEquilateral() {
+    if (this.side1 === this.side2 && this.side1 === this.side3) return true;
+  }
+
+  // no sides equal
+  isScalene() {
+    if (this.side1 !== this.side2 && this.side2 !== this.side3) return true;
+    return false;
+  }
+
+  checkTriangle() {
+    if (this.isNotTriangle()) {
+      return "Not a triangle!";
+    } else if (this.isIsosceles()) {
+      return "Isosceles";
+    } else if (this.isEquilateral()) {
+      return "Equilateral";
+    } else if (this.isScalene()) {
+      return "Scalene";
+    } else {
+      return "You broke it...";
+    }
   }
 };
-
-Triangle.prototype.isIsosceles = function() {
-  if ((this.side1 === this.side2 && this.side2 !== this.side3) || (this.side1 === this.side3 && this.side2 !== this.side3) ||
-  (this.side1 !== this.side2 && this.side2 === this.side3)) return true;
-  return false;
-};
-
-// all sides equal
-Triangle.prototype.isEquilateral = function() {
-  if (this.side1 === this.side2 && this.side1 === this.side3) return true;
-}
-
-// no sides equal
-Triangle.prototype.isScalene = function() {
-  if (this.side1 !== this.side2 && this.side2 !== this.side3) return true;
-  return false;
-}
-
-Triangle.prototype.checkTriangle = function() {
-  if (this.isNotTriangle()) {
-    return "Not a triangle!";
-  } else if (this.isIsosceles()) {
-    return "Isosceles";
-  } else if (this.isEquilateral()) {
-    return "Equilateral";
-  } else if (this.isScalene()) {
-    return "Scalene";
-  } else {
-    return "You broke it...";
-  }
-}
